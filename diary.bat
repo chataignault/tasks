0d359eafb994460c8412184229e94c57df066a18
CHATAIGNAULT <lc1823@ic.ac.uk>
2023-12-24 02:47:54 +0000

    folder creation and variable wrapping

--- diary.bat
+++ diary.bat
@@ -1,8 +1,21 @@
 @echo off
 setlocal
 
+set "basefolder=C:\code\tasks"
+
+if exist "%baseFolder%" (
+	cd "%baseFolder%"
+) else (
+	echo The root folder is not well specified
+	goto abortmessage
+)
+
+if NOT exist "%baseFolder%\diary" (
+	echo Create diary files folder
+	mkdir "%baseFolder%\diary"
+)	
 REM Set the folder path where you want to create the new file
-set "folderPath=C:\code\tasks\diary"
+set "folderPath=%baseFolder%\diary"
 
 REM Get the current date in the format YYYYMMDD
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do set "dateStamp=%%c%%b%%a"
@@ -13,7 +26,16 @@ set "fileName=%dateStamp%.txt"
 REM Combine the folder path and file name to get the full path
 set "fullPath=%folderPath%\%fileName%"
 
+REM Write file header
+printf "Record for  %dateStamp% :\n\n" > %fullPath%
+
 REM Start vim with the specified folder and new file
 vim "%fullPath%"
 
 endlocal
+
+exit
+
+:abortmessage
+echo The process has failed
+
