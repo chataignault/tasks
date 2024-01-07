e874204cd6ad487ddaa2cd36c404dcaefcb0759e
chataignault <leonchat@orange.fr>
2024-01-07 19:55:37 +0000

    copy lectures tex files to repo

---
+++ extract_tex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copy lecture notes from Imperial folder immediately to repository
+
+sourcePath="$cimp"
+destinationPath="/c/code/latex/cimp"
+
+
+if [ ! -d "$sourcePath" ]; then
+    echo "Source folder does not exist."
+    exit 1
+fi
+
+# Check if destination folder exists, create if not
+if [ ! -d "$destinationPath" ]; then
+    mkdir -p "$destinationPath"
+fi
+
+find "$sourcePath" -type f -name '*.tex' | while IFS= read -r sourceFile; do
+	relativePath=$(realpath --relative-to "$sourcePath" "$sourceFile") 
+	destinationFile="$destinationPath/$relativePath"
+
+	# Create folder if it does not already exist
+	destinationFolder=$(dirname "$destinationFile")
+	mkdir -p "$destinationFolder"
+
+	cp "$sourceFile" "$destinationFile"
+	echo "Copied $sourceFile to $destinationFile"
+done
+
+echo "Copy operation completed."
+
