e216cdd5916abee01ada8f54ac65832d1711c814
chataignault <leonchat@orange.fr>
2024-10-13 19:59:51 +0100

    add naming to TodoList

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -11,67 +11,22 @@ struct Todo {
 
 #[derive(Serialize, Deserialize)]
 struct TodoList {
+    name: String,
     items: HashMap<usize, Todo>,
     next_id: usize,
 }
 
 #[derive(Serialize, Deserialize)]
-struct TodoListTopics {
-    items: HashMap<usize, Todo>,
-    next_id: usize,
+struct TodoListTopic {
+    items: HashMap<String, TodoList>,
+    current_topic: String,
 }
 
-impl TodoListTopics {
-    fn new() -> Self {
-        TodoList {
-            items: HashMap::new(),
-            next_id: 1,
-        }
-    }
-
-    fn add(&mut self, topic: String, description: String) {
-        let todo = Todo {
-            description,
-            completed: false,
-        };
-        self.items[topic].insert(self.next_id[topic], todo);
-        self[topic].next_id += 1;
-        self.save_to_file();
-    }
-
-    fn mark_complete(&mut self, id: usize) -> bool {
-        if let Some(todo) = self.items.get_mut(&id) {
-            todo.completed = true;
-            self.save_to_file();
-            true
-        } else {
-            false
-        }
-    }
-
-    fn display(&self) {
-        for (id, todo) in &self.items {
-            let status = if todo.completed { "[x]" } else { "[ ]" };
-            println!("{} {}: {}", id, status, todo.description);
-        }
-    }
-
-    fn save_to_file(&self) {
-        let serialized = serde_json::to_string(self).unwrap();
-        fs::write("todo_list.json", serialized).expect("Unable to write file");
-    }
-
-    fn load_from_file() -> Self {
-        match fs::read_to_string("todo_list.json") {
-            Ok(contents) => serde_json::from_str(&contents).unwrap_or_else(|_| TodoList::new()),
-            Err(_) => TodoList::new(),
-        }
-    }
-}
 
 impl TodoList {
-    fn new() -> Self {
+    fn new(name: String) -> Self {
         TodoList {
+            name: name,
             items: HashMap::new(),
             next_id: 1,
         }
@@ -106,20 +61,73 @@ impl TodoList {
 
     fn save_to_file(&self) {
         let serialized = serde_json::to_string(self).unwrap();
-        fs::write("todo_list.json", serialized).expect("Unable to write file");
+        fs::write(format!("{}_todo_list.json", self.name), serialized).expect("Unable to write file");
     }
 
-    fn load_from_file() -> Self {
-        match fs::read_to_string("todo_list.json") {
-            Ok(contents) => serde_json::from_str(&contents).unwrap_or_else(|_| TodoList::new()),
-            Err(_) => TodoList::new(),
+    fn load_from_file(name: String) -> Self {
+        match fs::read_to_string(format!("{}_todo_list.json", name)) {
+            Ok(contents) => serde_json::from_str(&contents).unwrap_or_else(|_| TodoList::new(name)),
+            Err(_) => TodoList::new(name),
         }
     }
 }
 
+
+// impl TodoListTopic {
+//     fn new() -> Self {
+//         TodoListTopic {
+//             items: HashMap::new(),
+//             current_topic: "",
+//         }
+//     }
+
+//     fn add(&mut self, description: String) {
+//         let todo = Todo {
+//             description,
+//             completed: false,
+//         };
+//         self.items.get_mut(&self.current_topic).unwrap().items.insert(self.current_id, todo);
+//         self.save_to_file();
+//     }
+
+//     fn mark_complete(&mut self, id: usize) -> bool {
+//         if let Some(todo) = self.items.get_mut(self.current_topic).items.get_mut(&id) {
+//             todo.completed = true;
+//             self.save_to_file();
+//             true
+//         } else {
+//             false
+//         }
+//     }
+
+//     fn display(&self) {
+//         println!("{} :", self.current_topic);
+//         for (id, todo) in &self.get_mut(&self.current_topic).items {
+//             let status = if todo.completed { "[x]" } else { "[ ]" };
+//             println!("{} {}: {}", id, status, todo.description);
+//         }
+//     }
+
+//     fn save_to_file(&self) {
+//         self.items.get_mut(&self.current_topic).save_to_file();
+//         // let serialized = serde_json::to_string(self).unwrap();
+//         // fs::write("todo_list.json", serialized).expect("Unable to write file");
+//     }
+
+//     fn load_from_file() -> Self {
+//         // Read all of the topics : create folder to gather them
+//         match fs::read_to_string("todo_list.json") {
+//             Ok(contents) => serde_json::from_str(&contents).unwrap_or_else(|_| TodoList::new()),
+//             Err(_) => TodoList::new(),
+//         }
+//     }
+// }
+
+
 fn main() {
-    let mut todo_list = TodoList::load_from_file();
-    let String current_topic = "";
+    let name: String = "test".to_string();
+    let mut todo_list = TodoList::load_from_file(name);
+    // let current_topic =  String::new();
 
     loop {
         print!("Enter command (add/complete/list/change/quit): ");
@@ -156,20 +164,20 @@ fn main() {
             "list" => {
                 println!("Current TODO list:");
                 // if current_topic is an empty string then display all topics and tasks
-                todo_list.display(current_topic);
+                todo_list.display();
             }
-            "change" => {
-                println!("Current Topics:");
-                todo_list.display_topics();
-                println!("Choose existing or create a new one.")
-                io::stdin().read_line(&mut current_topic).unwrap();
-            }
-            "clear" => {
-                // if all tasks are marked completed, then delete the topic
-                // else remove the items if they are marked as completed
-                todo_lsit.clear_topic(current_topic)
-                println!("Topic {} cleared", current_topic)
-            }
+            // "change" => {
+            //     println!("Current Topics:");
+            //     todo_list.display_topics();
+            //     println!("Choose existing or create a new one.")
+            //     io::stdin().read_line(&mut current_topic).unwrap();
+            // }
+            // "clear" => {
+            //     // if all tasks are marked completed, then delete the topic
+            //     // else remove the items if they are marked as completed
+            //     todo_lsit.clear_topic(current_topic)
+            //     println!("Topic {} cleared", current_topic)
+            // }
             "quit" => {
                 println!("Goodbye!");
                 break;
