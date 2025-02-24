00131c1bbe2d89d220f1528cde37f1d1082938a5
chataignault <leonchat@orange.fr>
2025-02-24 11:45:45 +0000

    parallelise gather notes

--- bin/gather_pdf_notes.sh
+++ bin/gather_pdf_notes.sh
@@ -3,31 +3,41 @@
 # Copy pdf note rendering from Imperial folder to unified folder
 
 sourcePath="$cimp"
-outPath="$sourcePath/all_notes"
+destPath="$sourcePath/all_notes"
 
 if [ ! -d "$sourcePath" ]; then
     echo "Source folder does not exist."
     exit 1
 fi
 
-if [ ! -d "$outPath" ]; then
+if [ ! -d "$destPath" ]; then
 	echo "Create folder to gather notes"
-	mkdir "$outPath"
+	mkdir "$destPath"
 fi
 
-find "$sourcePath" -type f -name '*.tex' | while IFS= read -r sourceFile; do 
-	courseName=$(echo $(realpath --relative-to "$sourcePath" "$sourceFile") | awk -F/ '{print $1}')
-	pdfFile=$(echo "$sourceFile" | awk -F. '{print $1}')".pdf"
-	echo $(dirname "$pdfFile")
-	echo 
+copy_pdf_file() {
+	courseName=$(echo $(realpath --relative-to "$2" "$1") | \
+		awk -F/ '{print $1}')
+	pdfFile=$(echo "$1" | awk -F. '{print $1}')".pdf"
+	# echo $(dirname "$pdfFile")
+	# echo 
 	fileName=$(basename $(dirname "$pdfFile"))
-	echo "$fileName"
-	echo
-	destinationFile="$outPath/${courseName}_${fileName}.pdf"
-	
+	# echo "$fileName"
+	# echo
+	destinationFile="$3/${courseName}_${fileName}.pdf"
+	echo "$pdfFile" "$destinationFile"	
 	cp "$pdfFile" "$destinationFile"
-	echo "Copied $pdfFile to $destinationFile"
-done
+	# echo "Copied $pdfFile to $destinationFile"
+
+}
+
+export -f copy_pdf_file
+
+find "$sourcePath" -type f -name '*.tex' | \
+	xargs -P 10 -I {} bash -c 'copy_pdf_file "$@"' _ {} $sourcePath $destPath
+	# while IFS= read -r sourceFile; do 
+	# copy_file $sourceFile	
+#done
 
 echo "All note files copied to $outPath"
 
