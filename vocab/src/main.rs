830b2998f33d6f7c7f1c4f3617d0f3f4b9d11755
chataignault <leonchat@orange.fr>
2025-03-10 22:43:48 +0000

    start vocab tool

---
+++ vocab/src/main.rs
@@ -0,0 +1,8 @@
+use polars::prelude::*;
+
+fn main() {
+    // CLI user interface to :
+    // add new def to dictionary
+    // query defs from dictionary (sample at random)
+    // save the dictionary when leaving in csv file
+}
