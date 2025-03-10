9f0c1764ce59525103146e4c3a12e44d3b4e7239
chataignault <leonchat@orange.fr>
2025-03-10 23:01:05 +0000

    vocab add cli interface

--- vocab/src/main.rs
+++ vocab/src/main.rs
@@ -1,8 +1,38 @@
 use polars::prelude::*;
+use polars_core::prelude::*;
+use polars_io::prelude::*;
+use std::fs::File;
+use std::io;
 
 fn main() {
     // CLI user interface to :
     // add new def to dictionary
     // query defs from dictionary (sample at random)
     // save the dictionary when leaving in csv file
+    let mut file: String = String::from("vocab.csv");
+
+    loop {
+        print!("Enter action (add, ask, quit):");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        let mut df = DataFrame::default();
+
+        match input {
+            "add" => {}
+            "ask" => {}
+            "quit" => {
+                let mut file = File::create(file).expect("could not create file");
+
+                CsvWriter::new(&mut file)
+                    .include_header(true)
+                    .with_separator(b',')
+                    .finish(&mut df);
+                break;
+            }
+            _ => println!("Unknown command. Please try again."),
+        }
+    }
 }
