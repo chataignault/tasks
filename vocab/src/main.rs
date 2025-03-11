3985eaccaf78db15ef419105cff0e9a6b40cf8d4
chataignault <leonchat@orange.fr>
2025-03-11 21:13:08 +0000

    vocab dataframe creation

--- vocab/src/main.rs
+++ vocab/src/main.rs
@@ -1,8 +1,11 @@
 use polars::prelude::*;
 use polars_core::prelude::*;
+use polars_io::csv::read::CsvReadOptions;
+use polars_io::csv::write::CsvWriterOptions;
 use polars_io::prelude::*;
 use std::fs::File;
 use std::io;
+use std::path::Path;
 
 fn main() {
     // CLI user interface to :
@@ -11,6 +14,19 @@ fn main() {
     // save the dictionary when leaving in csv file
     let mut file: String = String::from("vocab.csv");
 
+    let mut voc = DataFrame::default();
+
+    if Path::new(&file.to_string()).exists() {
+        voc = CsvReadOptions::default()
+            .with_has_header(true)
+            .try_into_reader_with_file_path(Some(file.clone().into()))
+            .unwrap()
+            .finish()
+            .unwrap();
+        println!("Found current voc : len {}", voc.shape().0);
+        println!("{}", voc.head(Some(10)));
+    }
+
     loop {
         print!("Enter action (add, ask, quit):");
 
@@ -18,21 +34,29 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         let input = input.trim();
 
-        let mut df = DataFrame::default();
-
         match input {
             "add" => {}
             "ask" => {}
             "quit" => {
                 let mut file = File::create(file).expect("could not create file");
 
-                CsvWriter::new(&mut file)
+                let _ = CsvWriter::new(&mut file)
                     .include_header(true)
                     .with_separator(b',')
-                    .finish(&mut df);
+                    .finish(&mut voc);
                 break;
             }
             _ => println!("Unknown command. Please try again."),
         }
     }
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn test_add_description() {}
+
+    #[test]
+    fn test_initialise_df() {}
 }
