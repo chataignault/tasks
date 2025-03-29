3e7f50ef718b2bae208439691def0cee8dedabb5
chataignault <leonchat@orange.fr>
2025-03-29 23:48:48 +0000

    comment errors

--- todo-list/src/utils.rs
+++ todo-list/src/utils.rs
@@ -1,33 +1,31 @@
 use std::fs;
 use std::path::Path;
 
-static CACHE_FOLDER: String = "todos".to_string();
-static DEFAULT_TOPIC: String = "general".to_string();
+const CACHE_FOLDER: &str = "todos";
+const DEFAULT_TOPIC: &str = "general";
 
-
-
-pub fn load_from_file() -> Self {
+pub fn load_from_file() -> () {
     let path = Path::new(&CACHE_FOLDER);
-    let entries = match fs::read_dir(path) {
-        Ok(entries) => entries,
-        Err(_) => {
-            println!("Error reading directory");
-            return TodoList::new(&[], &[]);
-        }
-    };
-    let mut todo_list_topic = TodoList::new(&[], &[]);
-    for entry in entries {
-        if let Ok(entry) = entry {
-            if let Some(file_name) = entry.file_name().to_str() {
-                if let Some(topic) = file_name.split("_").next() {
-                    println!("Found topic : {}", topic);
-                    todo_list_topic.items.insert(
-                        (*topic).to_string(),
-                        TodoList::load_from_file((*topic).to_string()),
-                    );
-                }
-            }
-        }
-    }
-    return todo_list_topic;
+    // let entries = match fs::read_dir(path) {
+    //     Ok(entries) => entries,
+    //     Err(_) => {
+    //         println!("Error reading directory");
+    //         return TodoList::new(&[], &[]);
+    //     }
+    // };
+    // let mut todo_list_topic = TodoList::new(&[], &[]);
+    // for entry in entries {
+    //     if let Ok(entry) = entry {
+    //         if let Some(file_name) = entry.file_name().to_str() {
+    //             if let Some(topic) = file_name.split("_").next() {
+    //                 println!("Found topic : {}", topic);
+    //                 todo_list_topic.items.insert(
+    //                     (*topic).to_string(),
+    //                     TodoList::load_from_file((*topic).to_string()),
+    //                 );
+    //             }
+    //         }
+    //     }
+    // }
+    // return todo_list_topic;
 }
