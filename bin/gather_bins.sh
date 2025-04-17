b420733cdb9f5c5935eccbea1fcc9d410b21aed7
chataignault <leonchat@orange.fr>
2025-04-17 14:15:10 +0100

    start script to gather bins

---
+++ bin/gather_bins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# get the bin folder 
+
+# append bin folder to path
+
+# list the bash scripts already present in the bin floder
+
+# list the projects in the parent folder
+
+# if there is a cargo file
+# cargo build release 
+# move target .exe to bin folder
+
+# if it is shell or bat 
+# copy file
+
