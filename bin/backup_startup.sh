4dc289e8b31d970bec7112442704ff04b2bd2f41
chataignault <leonchat@orange.fr>
2025-08-20 15:03:13 +0100

    ubuntu write sync scripts

---
+++ bin/backup_startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# update local documents from backup partition to local 
+
+export BACKUP_FOLDER="/mnt/backup"
+export LECTURES_DEST="/home/leonc/cours"
+
+if [[ ! -d $BACKUP_FOLDER ]] ; then
+	echo "Backup folder is not mounted at this location : $BACKUP_FOLDER"
+	return
+fi
+
+if [[ ! -d "~/cours" ]] ; then
+	mkdir "/home/leonc/cours"
+fi
+
+# mirror synchronisation
+rsync -av --delete "$BACKUP_FOLDER/Cours/" $LECTURES_DEST
+
